7037fb99947af29717434129662289271a6cc4b3
Dafrok <o.o@mug.dog>
2016-04-20 12:01:49 +0800

    rewrite dom walker

--- src/lib/util.js
+++ src/lib/util.js
@@ -3,38 +3,25 @@ function getDOMNode(vm) {
     return vm.$el
 }
 
-// Walk DOM Tree and do something.
-function DOMTreeWalker(DOMNode, callback, scope = [], namespace = '') {
-    console.log(scope, namespace)
-    for (let key in DOMNode.children) {
-        if (!isNaN(+key)) {
-            const node = DOMNode.children[key]
-            let {scope: newScope, namespace: newNamespace} = getScope(node, scope, namespace)
-            callback && callback(node, newScope, newNamespace)
-            DOMTreeWalker(node, callback, newScope, newNamespace)
-        }
+function DOMTreeWalker(DOMNode, vueData = {}, scope = []) {
+    for (let i = 0; i < DOMNode.children.length; i++) {
+        const node = DOMNode.children[i]
+        const vueText = getVueText(node)
+        const {vueFor, scope} = getVueFor(node)
+        DOMTreeWalker(node, vueData, scope)
+        return Object.assign(vueData, vueText, vueFor)
     }
 }
 
-// Get scope of current node.
-function getScope(node, scope, namespace) {
+function getVueText(node) {
+    const vText  = node.getAttribute('v-text')
+    return vText ? {[vText]: node.innerText} : null
+}
+
+function getVueFor(node) {
     const expression = node.getAttribute('v-for')
     const inMatch = expression ? expression.match(/(.*) (?:in|of) (.*)/) : null
-    if (inMatch) {
-        return {
-            scope: scope.concat(inMatch[2].trim()),
-            namespace: inMatch[1].trim()
-        }
-    } else {
-        return  {
-            scope: scope.concat(),
-            namespace: namespace
-        }
-    }
+    return {vueFor: {[inMatch[2].trim()]: []}, scope: inMatch[1].trim()}
 }
 
-function getDirectiveTextExpression(vm) {
-    DOMTreeWalker(getDOMNode(vm), (node, scope,namespace) => {
-        const expression = node.getAttribute('v-text')
-    })
-}
+export DOMTreeWalker
--- test/main.js
+++ test/main.js
@@ -1,4 +1,19 @@
+import Vue from 'vue'
 import {forTest} from '../src/index.js'
+import {DOMTreeWalker} from '../src/lib/util.js'
+
+describe('DOM', function () {
+    it('walker', function () {
+        let vue = new Vue({
+            el: 'body',
+            template: '<div v-text="foo">baz</div>',
+            data: {
+                foo: 'bar'
+            }
+        })
+        expect()
+    })
+})
 
 describe("Just for test", function () {
     it("test", function () {
